da555b020daf873c30e71b7fac10ef2d85df4f33
Hugo Julien <hugo.julien@etu.univ-tours.fr>
2022-05-05 09:47:57 +0200

    Implementation of the back button action

--- src/main/java/fr/polytech/sircus/controller/PlayerMonitorController.java
+++ src/main/java/fr/polytech/sircus/controller/PlayerMonitorController.java
@@ -1,9 +1,17 @@
 package fr.polytech.sircus.controller;
 
+import fr.polytech.sircus.SircusApplication;
 import fr.polytech.sircus.model.Result;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Manages the interface used when the exam is in progress (player_monitor)
@@ -35,5 +43,16 @@ public class PlayerMonitorController {
     private void addComment(){
         this.result.addComment(this.commentTextArea.getText());
         this.commentTextArea.clear();
+    }
+
+    /**
+     * Go back to the previous page
+     */
+    public void previousPage() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Objects.requireNonNull(SircusApplication.class.getClassLoader().getResource("views/meta_seq.fxml")));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) playerMonitor.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
