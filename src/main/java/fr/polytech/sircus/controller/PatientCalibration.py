aebc04a2e71ffbcb5243296611bc1c3b648ed145
Guillaume <guillaume.cazin@hotmail.fr>
2022-07-11 16:46:36 +0200

    Feature: beginning of animate calibration

--- src/main/java/fr/polytech/sircus/controller/PatientCalibration.py
+++ src/main/java/fr/polytech/sircus/controller/PatientCalibration.py
@@ -1,13 +1,15 @@
 import distutils.util
 import random
 import sys
+import threading
 import time
 import tobiiresearch.implementation.EyeTracker
 import tobiiresearch.implementation.ScreenBasedCalibration
 from tkinter import *
-import threading
 from tobii_research_addons import ScreenBasedCalibrationValidation, Point2
 from screeninfo import get_monitors
+
+test = True
 
 # Both monitors have to have the same resolution
 screens = get_monitors()
@@ -22,13 +24,38 @@ root.geometry('%dx%d+%d+%d' % (secondScreen.width, secondScreen.height, secondSc
 root.overrideredirect(True)
 root.bind("<Escape>", lambda event: root.overrideredirect(False))
 
-canvas = Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight(), borderwidth=0,
-                highlightthickness=0, bg=str(sys.argv[3]))
+if not test:
+    canvas = Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight(), borderwidth=0,
+                    highlightthickness=0, bg=str(sys.argv[3]))
+else:
+    canvas = Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight(), borderwidth=0,
+                    highlightthickness=0, bg='white')
 canvas.pack()
 
 
 def create_circle(x, y, r, **kwargs):
     return canvas.create_oval(x - r, y - r, x + r, y + r, **kwargs)
+
+
+def plot(calibration_result):
+    for calibration_point in calibration_result.calibration_points:
+        position = calibration_point.position_on_display_area
+        create_circle(x=root.winfo_screenwidth() * position[0], y=root.winfo_screenheight() * position[1], r=20)
+        create_circle(x=root.winfo_screenwidth() * position[0], y=root.winfo_screenheight() * position[1], r=5,
+                      fill='black')
+        for calibration_sample in calibration_point.calibration_samples:
+            left_eye_position = calibration_sample.left_eye.position_on_display_area
+            right_eye_position = calibration_sample.right_eye.position_on_display_area
+            canvas.create_line(root.winfo_screenwidth() * position[0],
+                               root.winfo_screenheight() * position[1],
+                               root.winfo_screenwidth() * left_eye_position[0],
+                               root.winfo_screenheight() * left_eye_position[1],
+                               fill='red', width=1.5)
+            canvas.create_line(root.winfo_screenwidth() * position[0],
+                               root.winfo_screenheight() * position[1],
+                               root.winfo_screenwidth() * right_eye_position[0],
+                               root.winfo_screenheight() * right_eye_position[1],
+                               fill='green', width=1.5)
 
 
 def calibrateAndValidate():
@@ -41,8 +68,10 @@ def calibrateAndValidate():
     # Define the points on screen we should calibrate at.
     # The coordinates are normalized, i.e. (0.0, 0.0) is the upper left corner and (1.0, 1.0) is the lower right corner.
     points_to_calibrate = []
-    targetNumbers = int(sys.argv[1])
-    # targetNumbers = 5
+    if not test:
+        targetNumbers = int(sys.argv[1])
+    else:
+        targetNumbers = 5
     if targetNumbers == 2:
         points_to_calibrate = [(0.1, 0.1), (0.9, 0.9)]
     elif targetNumbers == 5:
@@ -51,17 +80,56 @@ def calibrateAndValidate():
         points_to_calibrate = [(0.5, 0.5), (0.1, 0.1), (0.5, 0.1), (0.9, 0.1), (0.1, 0.5), (0.9, 0.5), (0.1, 0.9),
                                (0.5, 0.9), (0.9, 0.9)]
 
-    randomizeTarget = bool(distutils.util.strtobool(sys.argv[2].capitalize()))
+    if not test:
+        randomizeTarget = bool(distutils.util.strtobool(sys.argv[2].capitalize()))
+    else:
+        randomizeTarget = True
     if randomizeTarget:
         random.shuffle(points_to_calibrate)
 
+    first_point = True
+    big_circle = None
     for point in points_to_calibrate:
-
         # Create target point with right color
-        create_circle(x=root.winfo_screenwidth() * point[0], y=root.winfo_screenheight() * point[1], r=20,
-                      fill=str(sys.argv[4]), outline=str(sys.argv[4]))
-        create_circle(x=root.winfo_screenwidth() * point[0], y=root.winfo_screenheight() * point[1], r=5, fill='black',
-                      outline='black')
+        if first_point:
+            if not test:
+                big_circle = create_circle(x=root.winfo_screenwidth() * point[0],
+                                           y=root.winfo_screenheight() * point[1], r=20,
+                                           fill=str(sys.argv[4]), outline=str(sys.argv[4]))
+            else:
+                big_circle = create_circle(x=root.winfo_screenwidth() * point[0],
+                                           y=root.winfo_screenheight() * point[1], r=20,
+                                           fill='black', outline='black')
+            little_circle = create_circle(x=root.winfo_screenwidth() * point[0],
+                                          y=root.winfo_screenheight() * point[1], r=5,
+                                          fill='black', outline='black')
+            first_point = False
+
+        print(point)
+        # Subtraction between big_circle current coordinates and point coordinate to know which direction taken
+        width_diff = round(canvas.coords(big_circle).__getitem__(0), 1) - (root.winfo_screenwidth() * point[0] - 20)
+        if width_diff > 0:
+            width_direction = -1
+        elif width_diff == 0:
+            width_direction = 0
+        else:
+            width_direction = 1
+
+        height_diff = round(canvas.coords(big_circle).__getitem__(1), 1) - (root.winfo_screenheight() * point[1] - 20)
+        if height_diff > 0:
+            height_direction = -1
+        elif height_diff == 0:
+            height_direction = 0
+        else:
+            height_direction = 1
+
+        while round(canvas.coords(big_circle).__getitem__(0), 1) != (root.winfo_screenwidth() * point[0] - 20):
+            canvas.move(big_circle, width_direction * root.winfo_screenwidth() / 500,
+                        height_direction * root.winfo_screenheight() / 500)
+            canvas.move(little_circle, width_direction * root.winfo_screenwidth() / 500,
+                        height_direction * root.winfo_screenheight() / 500)
+            time.sleep(0.0001)
+            canvas.update()
 
         # Wait a little for user to focus.
         time.sleep(0.7)
@@ -72,11 +140,21 @@ def calibrateAndValidate():
             # Not all eye tracker models will fail at this point, but instead fail on ComputeAndApply.
             calibration.collect_data(point[0], point[1])
 
-        # Erase each target point after calibration is done
-        create_circle(x=root.winfo_screenwidth() * point[0], y=root.winfo_screenheight() * point[1], r=20,
-                      fill=str(sys.argv[3]), outline=str(sys.argv[3]))
-        create_circle(x=root.winfo_screenwidth() * point[0], y=root.winfo_screenheight() * point[1], r=5,
-                      fill=str(sys.argv[3]), outline=str(sys.argv[3]))
+    # Erase target point at the end
+    if not test:
+        create_circle(x=round(canvas.coords(big_circle).__getitem__(0), 1) + 20,
+                      y=round(canvas.coords(big_circle).__getitem__(1), 1) + 20,
+                      r=20, fill=str(sys.argv[3]), outline=str(sys.argv[3]))
+        create_circle(x=round(canvas.coords(little_circle).__getitem__(0), 1) + 5,
+                      y=round(canvas.coords(little_circle).__getitem__(0), 1) + 5,
+                      r=5, fill=str(sys.argv[3]), outline=str(sys.argv[3]))
+    else:
+        create_circle(x=round(canvas.coords(big_circle).__getitem__(0), 1) + 20,
+                      y=round(canvas.coords(big_circle).__getitem__(1), 1) + 20,
+                      r=20, fill='white', outline='white')
+        create_circle(x=round(canvas.coords(little_circle).__getitem__(0), 1) + 5,
+                      y=round(canvas.coords(little_circle).__getitem__(0), 1) + 5,
+                      r=5, fill='white', outline='white')
 
     calibration_result = calibration.compute_and_apply()
     print("Compute and apply returned {0} and collected at {1} points.".
@@ -85,36 +163,49 @@ def calibrateAndValidate():
     # The calibration is done. Leave calibration mode.
     calibration.leave_calibration_mode()
 
-    points_to_validate = [Point2(0.3, 0.3), Point2(0.7, 0.3), Point2(0.3, 0.7), Point2(0.7, 0.7)]
+    '''points_to_validate = [Point2(0.3, 0.3), Point2(0.7, 0.3), Point2(0.3, 0.7), Point2(0.7, 0.7)]
 
-    with ScreenBasedCalibrationValidation(eyetracker, timeout_ms=3000) as calib:
+    with ScreenBasedCalibrationValidation(eyetracker, timeout_ms=3000, sample_count=30) as calib:
         for point in points_to_validate:
             # Create target point with right color
-            create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=20,
-                          fill=str(sys.argv[4]), outline=str(sys.argv[4]))
+            if not test:
+                create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=20,
+                              fill=str(sys.argv[4]), outline=str(sys.argv[4]))
+            else:
+                create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=20,
+                              fill='black', outline='black')
             create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=5,
                           fill='black', outline='black')
 
             calib.start_collecting_data(point)
             while calib.is_collecting_data:
-                time.sleep(0.7)
+                time.sleep(1.0)
 
             # Erase each target point after calibration is done
-            create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=20,
-                          fill=str(sys.argv[3]), outline=str(sys.argv[3]))
-            create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=5,
-                          fill=str(sys.argv[3]), outline=str(sys.argv[3]))
+            if not test:
+                create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=20,
+                              fill=str(sys.argv[3]), outline=str(sys.argv[3]))
+                create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=5,
+                              fill=str(sys.argv[3]), outline=str(sys.argv[3]))
+            else:
+                create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=20,
+                              fill='white', outline='white')
+                create_circle(x=root.winfo_screenwidth() * point.x, y=root.winfo_screenheight() * point.y, r=5,
+                              fill='white', outline='white')
 
-    calibration_result = calib.compute()
+    validation_result = calib.compute()
 
-    print(calibration_result.average_accuracy_left)
-    print(calibration_result.average_accuracy_right)
-    print(calibration_result.average_precision_left)
-    print(calibration_result.average_precision_right)
-    print(calibration_result.average_precision_rms_left)
-    print(calibration_result.average_precision_rms_right)
+    print(validation_result.average_accuracy_left)
+    print(validation_result.average_accuracy_right)
+    print(validation_result.average_precision_left)
+    print(validation_result.average_precision_right)
+    print(validation_result.average_precision_rms_left)
+    print(validation_result.average_precision_rms_right)'''
 
-    root.quit()
+    plot(calibration_result)
+
+    '''root.quit()
+    sys.exit()'''
 
 
 root.after(1000, threading.Thread(target=calibrateAndValidate).start())
